6f2f4098777c33294b4fee84e032c0453960f548
Tyler McGurrin <mcgurrintyler@gmail.com>
2024-10-30 17:55:23 -0400

    Fixed it so it works correctly

--- makeiso.sh
+++ makeiso.sh
@@ -1,15 +1,15 @@
 echo Installing Dependancies
 
-pacman -Syu python3 python-pytest pkgconf git sequoia-sq archiso micro grub base-devel
+sudo pacman -Syu python3 python-pytest pkgconf git sequoia-sq archiso micro grub base-devel
 
 echo Adding Keys
 
 git clone https://aur.archlinux.org/archlinux32-keyring.git
-
-makepkg -si ./archlinux-32-keyring/
-
+cd archlinux32-keyring
+makepkg -si --skippgpcheck
+cd ..
 
 echo Making ISO...
 mkdir -p out/
 mkdir -p work/
-mkarchiso -v -o out/ -w work/ -L Arch32ISO -m iso baseline/
+sudo mkarchiso -v -o out/ -w work/ -L Arch32ISO -P Xircon -m iso baseline/
