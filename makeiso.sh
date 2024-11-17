12c448c89e02764c9caf63200e72ccbd0447c9f4
Tyler McGurrin <mcgurrintyler@gmail.com>
2024-11-17 18:40:14 -0500

    e

--- makeiso.sh
+++ makeiso.sh
@@ -6,7 +6,7 @@ echo Adding Keys
 
 git clone https://aur.archlinux.org/archlinux32-keyring.git
 cd archlinux32-keyring
-runuser -u $nonprivilegeduser -c "makepkg -si --skippgpcheck --noconfirm"
+runuser -u "$nonprivilegeduser" -c "makepkg -si --skippgpcheck --noconfirm"
 cd ..
 sudo mv ./archlinux32-keyring/src/archlinux32-keyring*/archlinux32.pgp ./archlinux32-keyring/src/archlinux32-keyring*/pubring.pgp
 sudo pacman-key --import ./archlinux32-keyring/src/archlinux32-keyring*/pubring.pgp
